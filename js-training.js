ccfdd5155246a444e3977df6c1f4f504dc11f636
Alex <alekss.r@mail.ru>
2020-01-26 22:09:43 +0400

    continue training

--- js-training.js
+++ js-training.js
@@ -1,4 +1,26 @@
-let x = 23423;
-x = undefined;
-// x = String(x);
-alert(typeof x);
+// let language = prompt("What's the официальное название JavaScript?", "");
+//
+// if (language == 'ECMAScript') {
+//   alert("Верно");
+// } else  {
+//   alert("НЕ знаете?! ECMAScript!");
+// }
+
+//3
+let numb = prompt("Введите любое число", "");
+
+if (numb > 0) {
+  alert(1);
+} else if (numb < 0) {
+  alert(-1);
+} else if (numb == 0) {
+  alert(0);
+}
+
+// let year = prompt('В каком году появилась спецификация ECMAScript-2015?', '');
+//
+// if (year == 2015) {
+//   alert( 'Да вы знаток!' );
+// } else {
+//   alert( 'А вот и неправильно!' ); // любое значение, кроме 2015
+// }
