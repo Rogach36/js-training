78372e5055a29205ded129ad1161e74bb7c0d451
Alex <alekss.r@mail.ru>
2020-01-27 22:25:31 +0400

    training!

--- js-training.js
+++ js-training.js
@@ -1,3 +1,6 @@
+
+
+
 // let language = prompt("What's the официальное название JavaScript?", "");
 //
 // if (language == 'ECMAScript') {
@@ -18,14 +21,32 @@
 // }
 
 
-//4
-let result;
-
-if (a + b < 4) {
-  result = 'Мало';
-} else {
-  result = 'Много';
-}
+// //4
+// let result;
+//
+// if (a + b < 4) {
+//   result = 'Мало';
+// } else {
+//   result = 'Много';
+// }
+//let result = условие ? значение1 : значение2; (тернарный оператор?)
+//5
+// let message;
+//
+// if (login == 'Сотрудник') {
+//   message = 'Привет';
+// } else if (login == 'Директор') {
+//   message = 'Здравствуйте';
+// } else if (login == '') {
+//   message = 'Нет логина';
+// } else {
+//   message = '';
+// }
+//
+// let message = (login == 'Сотрудник') ? 'Привет':
+// (login == 'Директор') ? 'Здравствуйте':
+// (login == '') ? 'Нет логина':
+// '';
 
 //let result = (a + b < 4) ? 'Мало' : 'Много';
 
