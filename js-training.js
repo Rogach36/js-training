7c17bca695415935d31c0d1ea2e9009a24741908
Alex <49641838+Rogach36@users.noreply.github.com>
2020-01-27 17:34:18 +0400

    еще один пример (тернарый оператор)

--- js-training.js
+++ js-training.js
@@ -7,15 +7,30 @@
 // }
 
 //3
-let numb = prompt("Введите любое число", "");
+// let numb = prompt("Введите любое число", "");
+//
+// if (numb > 0) {
+//   alert(1);
+// } else if (numb < 0) {
+//   alert(-1);
+// } else if (numb == 0) {
+//   alert(0);
+// }
 
-if (numb > 0) {
-  alert(1);
-} else if (numb < 0) {
-  alert(-1);
-} else if (numb == 0) {
-  alert(0);
+
+//4
+let result;
+
+if (a + b < 4) {
+  result = 'Мало';
+} else {
+  result = 'Много';
 }
+
+//let result = (a + b < 4) ? 'Мало' : 'Много';
+
+
+
 
 // let year = prompt('В каком году появилась спецификация ECMAScript-2015?', '');
 //
